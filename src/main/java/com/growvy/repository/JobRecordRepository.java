b5d171cdc5d2eac920a8808735a9dd0629224cc1
yourm08 <s2413@e-mirim.hs.kr>
2026-02-04 04:06:27 +1100

    fix: modify JobRecord repository

--- src/main/java/com/growvy/repository/JobRecordRepository.java
+++ src/main/java/com/growvy/repository/JobRecordRepository.java
@@ -2,6 +2,8 @@ package com.growvy.repository;
 
 import com.growvy.entity.JobRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,5 +11,15 @@ public interface JobRecordRepository extends JpaRepository<JobRecord, Long> {
     // 필요시 findByApplicationId 등 추가 가능
     Optional<JobRecord> findByApplicationId(Long applicationId);
 
-    Optional<JobRecord> findByJobSeekerIdAndJobPostId(Long jobSeekerId, Long jobPostId);
+    @Query("""
+    SELECT r FROM JobRecord r
+    JOIN Application a ON a.id = r.applicationId
+    WHERE a.jobSeeker.user.id = :jobSeekerId
+      AND a.jobPost.id = :jobPostId
+""")
+    Optional<JobRecord> findByJobSeekerIdAndJobPostId(
+            @Param("jobSeekerId") Long jobSeekerId,
+            @Param("jobPostId") Long jobPostId
+    );
+
 }
